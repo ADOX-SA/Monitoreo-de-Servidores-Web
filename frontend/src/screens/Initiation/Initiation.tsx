eabdb6f88969fbb399382580b65842d3d053c059
Jose <furoox@gmail.com>
2024-08-16 16:24:04 -0300

    Fixes, Header and Footer

--- frontend/src/screens/Initiation/Initiation.tsx
+++ frontend/src/screens/Initiation/Initiation.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import styles from './Initiation.module.css'
-import { Dashboard } from '../Dashboard'
+"use client";
+import React from 'react';
+import styles from './Initiation.module.css';
+import { Dashboard } from '../Dashboard';
+import { Header } from '../Header';
+import { Footer } from '../Footer';
+
 
 function Initiation() {
   return (
     <div>
+        <Header/>
         <Dashboard/>
+        <Footer/>
     </div>
   )
 }
