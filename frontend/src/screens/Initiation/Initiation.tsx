890ef55ce45f5a483ee87acbdbcd6db6918571dd
Jose <furoox@gmail.com>
2024-08-16 16:31:18 -0300

    Fixes, Title

--- frontend/src/screens/Initiation/Initiation.tsx
+++ frontend/src/screens/Initiation/Initiation.tsx
@@ -4,12 +4,14 @@ import styles from './Initiation.module.css';
 import { Dashboard } from '../Dashboard';
 import { Header } from '../Header';
 import { Footer } from '../Footer';
+import { Title } from '../Title';
 
 
 function Initiation() {
   return (
     <div>
         <Header/>
+        <Title/>
         <Dashboard/>
         <Footer/>
     </div>
---
+++ frontend/src/screens/Title/Title.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Heading } from "@adoxdesarrollos/designsystem-2";
+
+function Title() {
+  return (
+    <Heading align="center" size="large">Monitoreo de Desarrollo</Heading>
+  )
+}
+
+export default Title;
